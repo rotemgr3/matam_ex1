d12f599ef564fb41fbce344bb6e4016e06bbd6c4
rotemgr3 <rotemg93@gmail.com>
2021-05-09 23:52:49 +0300

    Merge branch mater

---
+++ build/CMakeFiles/map_prog.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/home/rotem.green/mtm/ex1/wet/map.c" "/home/rotem.green/mtm/ex1/wet/build/CMakeFiles/map_prog.dir/map.c.o"
+  "/home/rotem.green/mtm/ex1/wet/map_example_test.c" "/home/rotem.green/mtm/ex1/wet/build/CMakeFiles/map_prog.dir/map_example_test.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/map_prog.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/map_prog.dir/map.c.o"
+  "CMakeFiles/map_prog.dir/map_example_test.c.o"
+  "map_prog"
+  "map_prog.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/map_prog.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()

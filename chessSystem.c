7e60be5a1294b8b8a1ffb7a4478bfb57eab4c77f
rotemgr3 <rotemg93@gmail.com>
2021-05-13 18:48:17 +0300

    update

---
+++ chessSystem.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+#include "map.h"
+#include "chessSystem.h"
+
+#define NUM_OF_PLAYRES_PER_GAME 2
+
+typedef struct Game {
+    int players_id[NUM_OF_PLAYRES_PER_GAME];
+
+
+
+}
